b45c13356d4764b0d553d30b2ef19261000920df
kimboseong <njunju77@nate.com>
2024-01-19 07:54:11 +0900

    add user

---
+++ src/main/kotlin/com/example/todoapp/domain/user/controller/UserController.kt
@@ -0,0 +1,34 @@
+package com.example.todoapp.domain.user.controller
+
+import com.example.todoapp.domain.user.dto.LoginDto
+import com.example.todoapp.domain.user.dto.SignUpDto
+import com.example.todoapp.domain.user.dto.UserResponseDto
+import com.example.todoapp.domain.user.service.UserService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+@RequestMapping("/users")
+@RestController
+class UserController(
+    private val userService: UserService
+) {
+    @PostMapping("/signUp")
+    fun signUp(
+      @RequestBody signUpDto: SignUpDto
+    ):ResponseEntity<UserResponseDto>{
+       return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(userService.signUp(signUpDto))
+    }
+    @PostMapping("/Login")
+    fun login(
+        @RequestBody loginDto: LoginDto
+    ):ResponseEntity<UserResponseDto>{
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(userService.login(loginDto))
+    }
+}
---
+++ src/main/kotlin/com/example/todoapp/domain/user/dto/LoginDto.kt
@@ -0,0 +1,6 @@
+package com.example.todoapp.domain.user.dto
+
+data class LoginDto(
+    val email:String,
+    val password:String,
+)
